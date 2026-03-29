2ebb235a4f7287b7393455bccb6ce1f8176a1738
Joicy dos Santos <joicyd.santosp@gmail.com>
2026-03-29 00:22:40 -0300

    Update pedido_route.py
    correção do update que subiu errado

--- backend/pedidos/pedido_route.py
+++ backend/pedidos/pedido_route.py
@@ -1,55 +1,47 @@
-from flask import request
+from flask import Blueprint, request, jsonify
+from .pedido_model import criar_carrinho, adicionar_item_ao_carrinho, Pedidos
 from config import db
-from pedido_model import Pedido
-from restaurante_model import Restaurante
-# from item_model import ItensPedido 
-# from produto_model import Produto
 
-def criarPedido():
-    dados = request.json
+pedidos_blueprint = Blueprint('pedidos', __name__)
+
+###cria um pedido vazio (Abre o carrinho)
+@pedidos_blueprint.route("/pedidos", methods=["POST"])
+def abrir_pedido():
+    dados = request.get_json()
     restaurante_id = dados.get('restaurante_id')
-    cliente_id = dados.get('cliente_id') # Importante saber quem pede
     
-    # VALIDAÇÃO DA EXISTENCIA
-    restaurante = Restaurante.query.get(restaurante_id)
-    if not restaurante:
-        return {"erro": "Restaurante não encontrado"}, 404
+    if not restaurante_id:
+         return jsonify({"erro": "O ID do restaurante é obrigatório"}), 400
 
-  
-    total_calculado = dados.get('total') # S
+    ###Chama a regra de negócio que está no Model
+    pedido, status = criar_carrinho(restaurante_id)
+    return jsonify(pedido), status
 
-    pedido = Pedido(
-        total=total_calculado, 
-        restaurante_id=restaurante_id,
-        cliente_id=cliente_id,
-        status='CRIADO' # Define um status inicial
-    )
+###adiciona um doce e calcula o faturamento
+@pedidos_blueprint.route("/pedidos/<int:pedido_id>/itens", methods=["POST"])
+def adicionar_item(pedido_id):
+    dados = request.get_json()
+    
+    # Passa o trabalho  pro Model fazer a matemática
+    pedido_atualizado, status = adicionar_item_ao_carrinho(pedido_id, dados)
+    return jsonify(pedido_atualizado), status
 
-    try:
-        db.session.add(pedido)
-        db.session.commit()
-        return pedido.to_dict(), 201
-    except Exception as e:
-        db.session.rollback()
-        return {"erro": "Erro ao criar pedido", "detalhe": str(e)}, 500
-
-
-
-def cancelarPedido(id):
-    pedido = Pedido.query.get(id)
+# rota cancelar
+@pedidos_blueprint.route("/pedidos/<int:id>/cancelar", methods=["PUT"])
+def cancelar_pedido(id):
+    pedido = db.session.get(Pedidos, id)
 
     if not pedido:
-        return {"erro": "Pedido não encontrado"}, 404
+        return jsonify({"erro": "Pedido não encontrado"}), 404
     
     if pedido.status == 'CANCELADO':
-        return {"erro": "Pedido já está cancelado"}, 400
+        return jsonify({"erro": "Pedido já está cancelado"}), 400
 
     try:
         pedido.status = 'CANCELADO'
-       
-        
         db.session.commit()
-        return {"mensagem": "Pedido cancelado com sucesso"}, 200
+        return jsonify({"mensagem": "Pedido cancelado com sucesso"}), 200
+    ##rollback no BD
     except Exception as e:
         db.session.rollback()
-        return {"erro": "Erro ao cancelar pedido"}, 500
+        return jsonify({"erro": "Erro ao cancelar pedido", "detalhes": str(e)}), 500
